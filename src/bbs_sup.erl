e5803cdfb1ba7c2824f5a2cb40f4bd264607b7c3
Yan Guiborat <yan.guiborat@kobil.com>
2020-11-10 15:21:30 +0100

    Starting bbs lobby

--- src/bbs_sup.erl
+++ src/bbs_sup.erl
@@ -29,7 +29,10 @@ init([]) ->
     SupFlags = #{strategy => one_for_all,
                  intensity => 10,
                  period => 1},
-    ChildSpecs = [{bubbles_sup, {bbs_agent_sup, start_link, []}, permanent, infinity, supervisor, [bbs_agent_sup]}],
+    ChildSpecs = [
+      {bubbles_sup, {bbs_agent_sup, start_link, []}, permanent, infinity, supervisor, [bbs_agent_sup]},
+      {bbs_lobby, {bbs_lobby, start_link, []}, permanent, infinity, worker, [bbs_agent_sup]}
+      ],
     {ok, {SupFlags, ChildSpecs}}.
 
 %% internal functions
