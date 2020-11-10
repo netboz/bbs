2faecfac1672bc1425873342c94e88760e7a971c
Yan Guiborat <yan.guiborat@kobil.com>
2020-11-10 15:23:42 +0100

    Adding goal predicate ( will be removed later )

--- priv/ontologies/ont_bbs_bubble.pl
+++ priv/ontologies/ont_bbs_bubble.pl
@@ -1,10 +1,10 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%% Initialisation %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
 action(initialize(Ns, Ag, Params), [], initialized(Ns, Ag, Params)).
-initialize(Ns, Ag, Params) :-
-    log(info,"Bubble ontology initialized",[]),
+
+initialize(AgentId, Namespace, Params) :-
     assert(initialized(Ns, Ag, Params)).
-
-
 
 action(boot(Ns, Ag, Params), [], booted(Ns, Ag, Params)).
 
@@ -12,4 +12,25 @@ boot(Ns, Ag, Params) :-
     log(info,"Bubble entering itself",[]),
     goal(inside_bubble(Ag)),
     assert(booted(Ns, Ag, Params)).
+
+
+
+goal(Goal) :-
+    log(debug, "Checking if  ~p",[Goal]),
+    call(Goal).
+
+goal(Goal) :-
+     log(info, "Looking to satisfy Goal: ~p", [Goal]),
+     action(Action, Prereq, Goal),
+     log(info, "prereqs ~p",[Prereq]),
+     satisfy_prereq(Prereq),
+     log(debug,"Prereqs ok performing action :~p",[{Onto,Action}]),
+     call(Action).
+
+satisfy_prereq([]).
+
+satisfy_prereq([Goal1|Others]) :-
+     goal(Goal1),
+     satisfy_prereq(Others).
+
 
