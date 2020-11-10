5f1524f8203e5843d9d9ad6614cd2ed7debbdc13
Yan Guiborat <yan.guiborat@kobil.com>
2020-11-10 15:25:41 +0100

    Cosmetic changes

--- src/bbs_app.erl
+++ src/bbs_app.erl
@@ -16,22 +16,22 @@
 -export([new_bubble/1, new_bubble_test/0]).
 
 start(_StartType, _StartArgs) ->
-    register(bbs, self()),
-    % Create ontologies index
-    ets:new(?ONTO_STORE,
-	    [named_table, set, protected,
-	     {read_concurrency, true}]),
-    %% Register ontologies
-    Ontolist = case application:get_env(bbs, ontologies) of
-		 undefined ->
-		     ?WARNING_MSG("No ontology registered", []), [];
-		 {ok, Value} -> Value
-	       end,
-    bbs_ontology:register_ontologies(Ontolist),
+  register(bbs, self()),
+  % Create ontologies index
+  ets:new(?ONTO_STORE,
+    [named_table, set, protected,
+      {read_concurrency, true}]),
+  %% Register ontologies
+  Ontolist = case application:get_env(bbs, ontologies) of
+               undefined ->
+                 ?WARNING_MSG("No ontology registered", []), [];
+               {ok, Value} -> Value
+             end,
+  bbs_ontology:register_ontologies(Ontolist),
 
-               
-    % Start application supervisor
-    bbs_sup:start_link().
+
+  % Start application supervisor
+  bbs_sup:start_link().
 
 stop(_State) -> ok.
 
@@ -44,13 +44,14 @@ new_bubble(#agent{} = Bubble_specs) ->
 %% internal functions
 
 
-
 %% Unit tests
 
 new_bubble_test() ->
-    BubSpecs = #agent{name = <<"bubble">>, aid_entries = [],
-		      startup_onts =
-			     [
-                {ontology, <<"bbs:bubble">>, []}
-                ]},
-    new_bubble(BubSpecs).
+  BubSpecs = #agent{name = <<"bubble">>, aid_entries = [],
+    startup_onts =
+    [
+      {ontology, <<"bbs:brain_tests">>, [], bbs_db_ets},
+      {ontology, <<"bbs:bubble">>, [], bbs_db_ets},
+      {ontology, <<"bbs:agent">>, [], bbs_db_ets}
+    ]},
+  new_bubble(BubSpecs).
