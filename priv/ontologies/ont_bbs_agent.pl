628abbbeb2b113558b52715bd2adae5a7608eacf
yan.guiborat <yan.guiborat@kobil.com>
2021-05-17 22:51:45 +0200

    Fixing unneeded backtracing in messaging

--- priv/ontologies/ont_bbs_agent.pl
+++ priv/ontologies/ont_bbs_agent.pl
@@ -36,10 +36,14 @@ process_agent_event(Type, Message) :-
 do_process_agent_event(Type, Message) :-
     Head = event(Type, Message, Options),
     "bbs:agent:event_handlers"::clause(Head, Body),
-    "bbs:agent:event_handlers"::Head,
+    cutted_goal("bbs:agent:event_handlers"::Head),
     member(once, Options),
     "bbs:agent:event_handlers"::retract(( Head :- Body )),
+    log(info,"FINISHED",[]),
     fail.
+
+cutted_goal(Goal) :-
+    Goal,!.
 
 %% @doc: react_on(+Event_type, +Event_patten, +Ontology_reaction, +Predicate)
 %% Set a hook to gen_fsm:handle(Type, Event_patten, _State) that prove Ontology_reaction:Predicate
@@ -59,47 +63,54 @@ react_on(Event_type, Event_patten, Ontology_reaction, Predicate, Options) :-
 %% Set an Ontology::predicate to be called when stim StimOnt::StimPred is triggered
 
 react_on_stim(StimOnt, StimPredicate, ReactionOnt, ReactionPredicate) :-
-    react_on(StimOnt, StimPredicate, ReactionOnt, ReactionPredicate, []).
+    react_on_stim(StimOnt, StimPredicate, ReactionOnt, ReactionPredicate, []).
 
 react_on_stim(StimOnt, StimPredicate, ReactionOnt, ReactionPredicate, Options) :-
-    "bbs:agent:stims"::assert(( stim(StimPredicate, StimPredicate, Options) :-
+    "bbs:agent:stims"::assert(( stim(StimOnt, StimPredicate, Options) :-
     ReactionOnt::ReactionPredicate )).
 
 %% @doc: stim_processed(+StimOnt, +StimMessage)
 %% execute all stored reactions liked to StimOnt::StimMessage
 
-action(process_stim(StimOnt, StimMessage), [log(info,"Processing stim",[])], stim_processed(StimOnt, StimMessage)).
+action(process_stim(StimOnt, StimMessage), [log(info,"Processing stim :~p",[StimMessage])], stim_processed(StimOnt, StimMessage)).
 
 %% @doc: process_stim(+StimOnt, +StimMessage)
 %% Effectively execute stim reactions and those that needs to be.
 
 process_stim(StimOnt, StimMessage) :-
-    \+do_process_stim(StimOnt, StimMessage).
+                              \+do_process_stim(StimOnt, StimMessage).
+
+
 
 do_process_stim(StimOnt, StimMessage) :-
     Head = stim(StimOnt, StimMessage, Options),
+              log(info, "Head : ~p", [Head]),
+
     "bbs:agent:stims"::clause(Head, Body),
-    "bbs:agent:stims"::Head,
+
+    cutted_goal("bbs:agent:stims"::Head),
+                      log(info, "checing option ", []),
+
     member(once, Options),
+                          log(info, "retracting option ", []),
+
     "bbs:agent:stims"::retract(( Head :- Body )),
     fail.
+
+
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%% Messaging related predicate %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-action(process_incoming_message(Com_Channel, From, To, Ontology, Predicate),[me(To)],
-    incoming_message_processed(Com_Channel, message(From, To, Ontology, Predicate))).
+action(process_incoming_message(Com_Channel, From, To, Ontology, Predicate),[],
+    incoming_message_processed(Com_Channel, From, To, Ontology, Predicate)).
 
-%action(process_incoming_message(Com_Channel, From, To, Ontology, Predicate),[message_transport_ontology(Transport), cc(CcId, To)],
-%    incoming_message_processed(Com_Channel, message(From, To, Ontology, Predicate))).
+process_incoming_message(Cc, From, To, Ontology, Predicate) :-
+    goal(stim_processed("bbs:agent", message(Cc, From, To, Ontology, Predicate))).
 
 action(Transport_Ontology::goal(sent_message(CcId, message(To, Ontology, Predicate))),
          [message_transport_ontology(Transport_Ontology), Transport_Ontology::cc(To, CcId, Ontology)],
             sent(CcIc, message(To, Ontology, Predicate))).
-
-process_incoming_message(Cc, From, To, Ontology, Predicate) :-
-    log(info,"Processing message :~p", [Predicate]),
-    trigger_stims("bbs:agent", incoming_message(Cc, message(From, To, Ontology, Predicate))).
-
 
 
