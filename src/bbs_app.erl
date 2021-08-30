fe504033c22ef42334537bd8ea74f12e1ed7c08f
Yan Guiborat <yan.guiborat@gmail.com>
2021-08-31 01:18:39 +0200

    Small refactoring

--- src/bbs_app.erl
+++ src/bbs_app.erl
@@ -6,18 +6,18 @@
 -module(bbs_app).
 
 -include("bbs.hrl").
--include("utils.hrl").
 
 -behaviour(application).
 
 -export([start/2, stop/1, hostname/0]).
--export([new_bubble/1, new_bubble_test/0]).
+-export([new_bubble/1]).
 
 -define(SWARM, 'Elixir.Swarm').
 
 start(_StartType, _StartArgs) ->
   register(bbs, self()),
   application:ensure_all_started(swarm),
+
   % Create ontologies index
   ets:new(?ONTO_STORE, [named_table, set, protected, {read_concurrency, true}]),
 
@@ -31,7 +31,10 @@ start(_StartType, _StartArgs) ->
   % Supervisation specs for root bubble
   StartUpOntologies =
     application:get_env(bbs, root_bubble_ontologies, default_root_bubble_ontologies()),
-  MotherBubChildSpecs = #agent{name = <<"root">>, parent = node(), startup_ontologies = StartUpOntologies},
+  MotherBubChildSpecs =
+    #agent{name = <<"root">>,
+           parent = node(),
+           startup_ontologies = StartUpOntologies},
   Root_bubble_specs =
     #{id => root_bubble,
       start => {bbs_agent, start_link, [MotherBubChildSpecs]},
@@ -40,7 +43,11 @@ start(_StartType, _StartArgs) ->
       type => worker,
       modules => [bbs_agent_sup]},
   % Next effectively start root bubble
-  {ok, _Pid} = ?SWARM:register_name({<<"root">>, node()}, supervisor, start_child, [AppSupPid, Root_bubble_specs]).
+  {ok, _Pid} =
+    ?SWARM:register_name({<<"root">>, node()},
+                         supervisor,
+                         start_child,
+                         [AppSupPid, Root_bubble_specs]).
 
 stop(_State) ->
   ok.
@@ -49,14 +56,16 @@ stop(_State) ->
 
 default_root_bubble_ontologies() ->
   [{ontology, <<"bbs:brain_tests:data">>, [], bbs_db_ets},
-    {ontology, <<"bbs:brain_tests">>, [], bbs_db_ets},
-    {ontology, <<"bbs:agent">>, [], bbs_db_ets},
-    {ontology, <<"bbs:mts:client:gproc">>, [], bbs_db_ets}
-    %{ontology, <<"bbs:bubble">>, [], bbs_db_ets},
-    %   {ontology, <<"bbs:mts:mqtt:broker">>, [], bbs_db_ets},
-   % {ontology, <<"bbs:mts:client:mqtt">>, [], bbs_db_ets},
-   % {ontology, <<"bbs:root">>, [], bbs_db_ets}
-   ].
+   {ontology, <<"bbs:brain_tests">>, [], bbs_db_ets},
+   {ontology, <<"bbs:agent">>, [], bbs_db_ets},
+   {ontology,
+    <<"bbs:mts:client:gproc">>,
+    [],
+    bbs_db_ets}].    
+    % {ontology, <<"bbs:bubble">>, [], bbs_db_ets},
+    % {ontology, <<"bbs:mts:mqtt:broker">>, [], bbs_db_ets},
+    % {ontology, <<"bbs:mts:client:mqtt">>, [], bbs_db_ets},
+    % {ontology, <<"bbs:root">>, [], bbs_db_ets}
 
 %% API
 
@@ -66,35 +75,9 @@ new_bubble(#agent{} = Bubble_specs) ->
 %% internal functions
 
 %% Unit tests
-
-new_bubble_test() ->
-  Id = base64:encode(crypto:strong_rand_bytes(8)),
-  BubSpecs =
-    #agent{name = Id,
-      startup_ontologies =
-      [% broken {ontology, <<"bbs:brain_tests">>, [], bbs_db_ets},
-        {ontology, <<"bbs:agent">>, [], bbs_db_ets},
-        %    {ontology, <<"bbs:bubble">>, [], bbs_db_ets},
-        {ontology, <<"bbs:mts:client:gproc">>, [], bbs_db_ets}
-        %    {ontology, <<"bbs:mts:mqtt:client">>, [], bbs_db_ets},
-        %    {ontology, <<"bbs:root">>, [], bbs_db_ets}]},
-      ]},
-  % Supervisation specs for root bubble
-
-  Root_bubble_specs =
-    #{ id => Id,
-      start => {bbs_agent, start_link, [BubSpecs]},
-      restart => permanent,
-      shutdown => infinity,
-      type => worker,
-      modules => [bbs_agent_sup]},
-
-  {ok, _Pid} =  ?SWARM:register_name(Id, supervisor, start_child, [bbs_sup, Root_bubble_specs]).
-
 hostname() ->
-  case {inet_db:gethostname(),inet_db:res_option(domain)} of
-    {H,D} when is_list(D), is_list(H),
-      length(D)> 0, length(H)>0 ->
+  case {inet_db:gethostname(), inet_db:res_option(domain)} of
+    {H, D} when is_list(D), is_list(H), length(D) > 0, length(H) > 0 ->
       H ++ "." ++ D;
     Other ->
       error({hostname, Other})
