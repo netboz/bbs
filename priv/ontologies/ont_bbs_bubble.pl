32b4b0d45c3aa4cab5734fdd8e9b52bff2a3d779
Yan Guiborat <yan.guiborat@gmail.com>
2021-12-02 14:30:58 +0100

    Enabling to choose bubble name and childs agents as params

--- priv/ontologies/ont_bbs_bubble.pl
+++ priv/ontologies/ont_bbs_bubble.pl
@@ -1,17 +1,25 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%% Actions %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 action(initialize(AgentId, Parent, NameSpace, Params), [
+    pairs_key_value(Params, bubble_name, BubbleName),
+    pairs_key_value(Params, bubble_childs, BubbleChilds),
+    initial_childs_started(BubbleChilds),
     registered(BubbleName),
     "bbs:agent"::react_on(info_event, child_down(AnyChild, Reason), "bbs:bubble", signal_process_exit(AnyChild, Reason),[]),
-    child("test_bob",[
-        ontology("bbs:agent", [], bbs_db_ets), 
-        ontology("bbs:mts:client:registry", [], bbs_db_ets),
-        ontology("bbs:mts:client:mqtt", [], bbs_db_ets)])
-    ],
         initialized(AgentId, Parent, NameSpace, Params)).
 
 initialize(AgentId, Parent, NameSpace, Params) :-
     assert(initialized(AgentId, Parent, NameSpace, Params)).
+
+
+pairs_key_value([{Key, Value}|_], Key, Value).
+pairs_key_value([{_OtherKey, _OtherValue}|OtherPairs], Key, Value) :-
+    pairs_key_value(OtherPairs, Key, Value).
+
+initial_childs_started([]]).
+initial_childs_started([child(Name, Ontologies)|OtherChilds]]) :-
+    goal(child(Name, Ontologies)),
+    initial_childs_started(OtherChilds).
 
 
 action(assert(child_bubble(BubbleName)), [register_bubble(BubbleName)], registered(BubbleName)).
@@ -33,7 +41,5 @@ action(terminate_child(Name), [], terminated(Name)).
 signal_process_exit(Name, Reason) :-
     child(Name, Ontologies),
     retract(child(Name, Ontologies)).
-
-
 
 
