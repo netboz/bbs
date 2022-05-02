51b31e02b10b7fe0e3ce26043725ce5527f38bf6
Yan Guiborat <yan.guiborat@gmail.com>
2022-05-02 18:35:36 +0200

    Moving configuration files to Elixir standard

--- config/config.exs
+++ config/config.exs
@@ -1,17 +1,3 @@
 # TODO: replace with Config when we will migrate to Elixir 1.9
-use Mix.Config
-
-config_file = case Mix.env do
-  # Config used in development environment
-  :test          -> "dev.config"
-  # Configuration used in production
-  :prod_         -> "prod.config"
-  # Configuration used in bitbucket pipeline
-  :pip           -> "pip.config"
-  # Default configuration : test
-  _              -> "dev.config"
-end
-
-inspect("Using configuration :#{inspect(config_file)}")
-{:ok, [sys_config]} = :file.consult(Path.join('config', config_file))
-for {app, config} <- sys_config, do: config(app, config)
+import Config
+import_config "#{config_env()}.config.exs"
