2de712670c2853cc8130eab6fd876120007bfc98
Yan Guiborat <yan.guiborat@gmail.com>
2022-05-02 17:49:00 +0200

    Refactoring config file selection

--- config/config.exs
+++ config/config.exs
@@ -1,17 +1,15 @@
 # TODO: replace with Config when we will migrate to Elixir 1.9
 use Mix.Config
 
-config_file = "app.config"
-
-case Mix.env do
+config_file = case Mix.env do
   # Config used in development environment
-  :test          -> config_file = "dev.config"
+  :test          -> "dev.config"
   # Configuration used in production
-  :prod_         -> config_file = "prod.config"
+  :prod_         -> "prod.config"
   # Configuration used in bitbucket pipeline
-  :pip           -> config_file = "pip.config"
+  :pip           -> "pip.config"
   # Default configuration : test
-  _              -> config_file = "dev.config"
+  _              -> "dev.config"
 end
 
 {:ok, [sys_config]} = :file.consult(Path.join('config', config_file))
