ad4dcbbf746c34478e0ca0eaea48be3ef1c990d3
Yan Guiborat <yan.guiborat@gmail.com>
2022-05-02 17:42:01 +0200

    Start installing CT

---
+++ ctest/ont_bbs_agent_SUITE.erl
@@ -0,0 +1,41 @@
+
+-module(ont_bbs_agent_SUITE).
+
+-include_lib("common_test/include/ct.hrl").
+-include_lib("erlog/include/erlog_int.hrl").
+
+%% Test server callbacks
+-export([all/0, init_per_testcase/2, end_per_testcase/2, init_per_suite/1,
+         end_per_suite/1]).
+-export([test_agent_pred/1]).
+-export([mocked_agent/2]).
+
+all() ->
+  [test_agent_pred].
+
+init_per_suite(Configdata) ->
+  application:ensure_all_started(bbs),
+  application:start(bbs),
+  Configdata.
+
+end_per_suite(_Configdata) ->
+  ok.
+
+init_per_testcase(_Test, Configdata) ->
+  Configdata.
+
+end_per_testcase(_Test, Configdata) ->
+  Configdata.
+
+
+
+
+
+mocked_agent(AgentName, ParentName) ->
+  gproc:reg({n, g, {AgentName, ParentName}}, unused),
+  receive
+    _ ->
+      ok
+  after 3000 ->
+    ok
+  end.
