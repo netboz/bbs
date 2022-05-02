54301bf82e90ba3d035912cf6d62e95679976ef2
Yan Guiborat <yan.guiborat@gmail.com>
2022-05-02 17:37:41 +0200

    Make configuration environment wise

--- config/config.exs
+++ config/config.exs
@@ -3,5 +3,14 @@ use Mix.Config
 
 config_file = "app.config"
 
+case Mix.env do
+  # Config used in development environment
+  :test          -> config_file = dev.config
+  # Configuration used in production
+  :prod_         -> config_file = prod.config
+  # Configuration used in bitbucket pipeline
+  :pip           -> config_file = pip.config
+end
+
 {:ok, [sys_config]} = :file.consult(Path.join('config', config_file))
 for {app, config} <- sys_config, do: config(app, config)
