9ad99e5f14fd2da13c043971b6e113b5359b07e8
Yan Guiborat <yan.guiborat@gmail.com>
2022-05-25 14:51:54 +0200

    Adding stims for bubble related events

--- priv/ontologies/ont_bbs_bubble.pl
+++ priv/ontologies/ont_bbs_bubble.pl
@@ -25,24 +25,41 @@ initial_childs_started([child(Name, Ontologies)|OtherChilds]) :-
     initial_childs_started(OtherChilds).
 
 
-action(assert(child_bubble(BubbleName)), [register_bubble(BubbleName)], registered(BubbleName)).
+action(assert(bubble(BubbleName)), 
+    [register_bubble(BubbleName),
+    "bbs:agent"::stim_processed("bbs:bubble", bubble_registered(BubbleName))
+    ], 
+    registered(BubbleName)).
 
 % @doc: Start a nea child process running given agent specs
-action(assert(child(Name, Ontologies)),[spawn_child(agent(Name, Ontologies))], child(Name, Ontologies)).
+action(assert(child(Name, Ontologies)),[
+    spawn_child(agent(Name, Ontologies)),
+    "bbs:agent"::goal(stim_processed("bbs:bubble", spawned(Name, Ontologies)))], 
+    child(Name, Ontologies)).
 
 % @doc: Stop child process ( process will terminate current goal and terminates )
-action(stop_child(Name), [], stopped(Name)).
+action( "bbs:agent":goal(stim_processed("bbs:bubble", stopped(Name, Ontologies))), 
+    [child(Name, Ontologies), 
+    stop_child(Name), 
+    retract(Name, Ontologies)], 
+        stopped(Name)).
 
 % @doc: Abruptly terminate child ( process stops any goal it was running and terminates )
-action(terminate_child(Name), [], terminated(Name)).
-
-
+action("bbs:agent"::goal(stim_processed("bbs:bubble", terminated(Name))), 
+    [child(Name, Ontologies), 
+    terminate_child(Name)], 
+        terminated(Name)).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Utilities %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
 signal_process_exit(Name, Reason) :-
     child(Name, Ontologies),
-    retract(child(Name, Ontologies)).
+    retract(child(Name, Ontologies)),
+    fail.
+
+signal_process_exit(Name, Reason) :-
+    "bbs:agent"::goal(stim_processed("bbs:bubble", exited(Name, Reason))).
+
 
 
